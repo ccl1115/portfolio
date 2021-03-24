cf56fa219e4aa0887cc342571bbea611fcacb164
禹璐 <yulu@chemi.ren>
2021-03-24 16:55:31 +0800

    add footer message

--- src/App.svelte
+++ src/App.svelte
@@ -132,23 +132,33 @@
     </div>
     <div
       id="footer"
-      class="py-10 mx-5 rounded-t-sm mb-20 bg-white text-gray-800 text-center shadow-lg"
+      class="py-10 lg:mx-5 rounded-t-sm lg:mb-20 bg-white text-gray-800 text-center shadow-lg"
     >
-      <p>© 2021 all rights reversed by Simon Yu</p>
-      <p class="text-xs">
-        This portfolio is written using <a
-          class="text-blue-700"
-          href="https://www.typescriptlang.org">TypeScript</a
-        >
-        and <a class="text-blue-700" href="https://svelte.dev">Svelte</a>
-      </p>
-      <p class="text-xs">
-        <a class="text-blue-700" href="https://greensock.com/gsap">GSAP</a> for animation
-      </p>
-      <p class="text-xs">
-        <a class="text-blue-700" href="https://tailwindcss.com">TailwindCSS</a> for
-        styling
-      </p>
+      <div class="grid sm:grid-cols-1 lg:grid-cols-3">
+        <p class="lg:col-span-3 sm:pb-0 lg:pb-10 lg:text-lg">
+          © 2021 all rights reversed by Simon Yu
+        </p>
+        <p class="text-xs">
+          <a
+            target="blank"
+            class="text-blue-700"
+            href="https://github.com/ccl1115/portfolio">Source code</a
+          > of this site
+        </p>
+        <p class="text-xs">
+          This portfolio is written using <a
+            class="text-blue-700"
+            href="https://www.typescriptlang.org">TypeScript</a
+          >
+          and <a class="text-blue-700" href="https://svelte.dev">Svelte</a>
+        </p>
+        <p class="text-xs">
+          <a class="text-blue-700" href="https://greensock.com/gsap">GSAP</a> for
+          animation and 
+          <a class="text-blue-700" href="https://tailwindcss.com">TailwindCSS</a
+          > for styling
+        </p>
+      </div>
     </div>
   </div>
 </main>
